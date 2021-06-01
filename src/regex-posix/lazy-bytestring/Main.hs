1298ee564bfc188e9e613a83d2f504e7dae64521
coberndorm <60147116+coberndorm@users.noreply.github.com>
2021-06-01 18:40:42 -0500

    Add files via upload

---
+++ src/regex-posix/lazy-bytestring/Main.hs
@@ -0,0 +1,19 @@
+import System.Environment
+import Text.Regex.Posix
+import qualified Data.ByteString.Lazy.Char8 as B
+import Control.Exception
+import Data.Time
+
+-- |Main Function
+main :: IO ()
+main = do
+            args <- getArgs
+            txt <- readFile (head args)
+            let regex = last args
+            start <- getCurrentTime
+            print ("Result: " ++ show (checkRegex txt regex :: Bool))
+            end <- getCurrentTime
+            print ("Time: " ++ show(diffUTCTime end start))
+
+checkRegex :: String -> String -> Bool
+checkRegex txt regex =  B.pack txt =~ B.pack regex :: Bool
